376f054244ce977c68cdc2c6b411453703259e5d
Umar Hussain <umarhussain771@gmail.com>
2022-08-01 10:16:28 +0100

    Complete tests 5/7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let str = string
+  let array = []
+  for(let i = 1; i <= num; i++){
+    array.push(str)
+  }
+  let arr = array.join('')
+  return arr
 };
 
 // Do not edit below this line
