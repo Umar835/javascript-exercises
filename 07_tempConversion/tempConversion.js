6017de872659578c1e4da95686fc79fae6fc5dc3
Umar Hussain <umarhussain771@gmail.com>
2022-08-02 10:16:18 +0100

    Complete tests for task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(fahrenheitNum) {
+ const celsiusConv = (fahrenheitNum - 32) * (5/9);
 
+ const celsiusRound = Math.round(celsiusConv * 10) / 10;
+
+ return celsiusRound
 };
 
-const ctof = function() {
+const ctof = function(celsiusNum) {
+const fahrenheitConv = (celsiusNum * (9/5) + 32);
 
+const fahrenheitRound = Math.round(fahrenheitConv * 10) / 10;
+
+return fahrenheitRound
 };
 
 // Do not edit below this line
