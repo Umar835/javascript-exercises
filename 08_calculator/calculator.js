53542ad35b3976054b883af7c3a287735db67ef5
Umar Hussain <umarhussain771@gmail.com>
2022-09-04 13:38:19 +0100

    Complete calculator task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+	return num.reduce((a, b)=> a + b, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  return num.reduce((a, b)=> a * b)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  //base case
+  if(num === 0 || num === 1){
+    return 1
+  // recursive case
+  }else {
+    return num * factorial(num-1)
+  }
 };
 
 // Do not edit below this line
