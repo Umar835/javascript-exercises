6138ccf6dfb1a2efb8efa91407b84705d027db20
Umar Hussain <umarhussain771@gmail.com>
2022-09-08 12:12:58 +0100

    Complete palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const updatedStr = str.toLowerCase().replace(/[^A-Za-z]/g, ""); 
+    //Removes uppercase letters and punctuation from string.
+    
+    const reverseStr = updatedStr.split('').reverse().join('')
+    //Reverses the string
 
+    return updatedStr === reverseStr ? true : false;
+    //checks if the string inputted matches the reversed string
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
